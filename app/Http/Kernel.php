eca3ef4b54cbcaeedff963a9552f536e29e865ea
zamroni <forheron@gmail.com>
2022-12-08 11:50:59 +0700

    create mail & jobs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => AdminMiddleware::class,
+        'module' => ModulePermissionMiddleware::class,
+        'api_lang' => APILocalizationMiddleware::class,
+        'maintenance_mode' => MaintenanceModeMiddleware::class,
     ];
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\CPU\Helpers;
-use App\Model\BusinessSetting;
+// use App\Model\BusinessSetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
@@ -34,35 +34,35 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrap();
-        try {
-            $web = BusinessSetting::all();
-            $settings = Helpers::get_settings($web, 'colors');
-            $data = json_decode($settings['value'], true);
-            $web_config = [
-                'primary_color' => $data['primary'],
-                'secondary_color' => $data['secondary'],
-                'name' => Helpers::get_settings($web, 'company_name'),
-                'phone' => Helpers::get_settings($web, 'company_phone'),
-                'web_logo' => Helpers::get_settings($web, 'company_web_logo'),
-                'mob_logo' => Helpers::get_settings($web, 'company_mobile_logo'),
-                'fav_icon' => Helpers::get_settings($web, 'company_fav_icon'),
-                'email' => Helpers::get_settings($web, 'company_email'),
-                'about' => Helpers::get_settings($web, 'about_us'),
-                'footer_logo' => Helpers::get_settings($web, 'company_footer_logo'),
-                'copyright_text' => Helpers::get_settings($web, 'company_copyright_text'),
-            ];
+        // try {
+        //     $web = BusinessSetting::all();
+        //     $settings = Helpers::get_settings($web, 'colors');
+        //     $data = json_decode($settings['value'], true);
+        //     $web_config = [
+        //         'primary_color' => $data['primary'],
+        //         'secondary_color' => $data['secondary'],
+        //         'name' => Helpers::get_settings($web, 'company_name'),
+        //         'phone' => Helpers::get_settings($web, 'company_phone'),
+        //         'web_logo' => Helpers::get_settings($web, 'company_web_logo'),
+        //         'mob_logo' => Helpers::get_settings($web, 'company_mobile_logo'),
+        //         'fav_icon' => Helpers::get_settings($web, 'company_fav_icon'),
+        //         'email' => Helpers::get_settings($web, 'company_email'),
+        //         'about' => Helpers::get_settings($web, 'about_us'),
+        //         'footer_logo' => Helpers::get_settings($web, 'company_footer_logo'),
+        //         'copyright_text' => Helpers::get_settings($web, 'company_copyright_text'),
+        //     ];
 
-            //language
-            $language = BusinessSetting::where('type', 'language')->first();
+        //     //language
+        //     $language = BusinessSetting::where('type', 'language')->first();
 
-            //currency
-            \App\CPU\Helpers::currency_load();
+        //     //currency
+        //     \App\CPU\Helpers::currency_load();
 
-            View::share(['web_config' => $web_config, 'language' => $language]);
+        //     View::share(['web_config' => $web_config, 'language' => $language]);
 
-            Schema::defaultStringLength(191);
-        } catch (\Exception $ex) {
+        //     Schema::defaultStringLength(191);
+        // } catch (\Exception $ex) {
 
-        }
+        // }
     }
 }
